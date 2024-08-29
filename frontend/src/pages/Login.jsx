8cafb710fe933290494968e7457b822bd858bfd2
Progetto2324NSD <review18r@gmail.com>
2024-08-29 13:25:30 +0200

    Update Login.jsx

--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -57,7 +57,11 @@ function Login() {
 
     try{
       // Chiamata API per la registrazione dell'utente
-      const response = await axios.post('/user/login', userData);
+      const response = await axios.post('/user/login', userData, {
+        withCredentials: true
+      });
+
+      localStorage.setItem('user', JSON.stringify(response.data));
 
       // Se la registrazione ha successo, mostra il caricamento
       // Mostra la rotellina di caricamento
