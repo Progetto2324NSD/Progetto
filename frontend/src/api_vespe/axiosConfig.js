9379fad9f99b00ad9b8c369920409b59a208cc6e
Progetto2324NSD <review18r@gmail.com>
2024-08-29 13:25:22 +0200

    Update axiosConfig.js

--- frontend/src/api_vespe/axiosConfig.js
+++ frontend/src/api_vespe/axiosConfig.js
@@ -1,10 +1,29 @@
 import axios from 'axios';
 
+// Crea un'istanza di Axios
 const instance = axios.create({
-  baseURL: 'http://localhost:5000',
+  baseURL: 'http://localhost:5000', // URL di base per le tue API
   headers: {
-    'Content-Type': 'application/json',
-  },
+    'Content-Type': 'application/json'
+  }
 });
+
+// Aggiungi un'interceptor di richiesta
+instance.interceptors.request.use(
+  (config) => {
+    // Ottieni il token dal localStorage
+    const token = localStorage.getItem('authToken');
+
+    // Se il token esiste, aggiungilo all'intestazione Authorization
+    if (token) {
+      config.headers['Authorization'] = `Bearer ${token}`;
+    }
+
+    return config;
+  },
+  (error) => {
+    return Promise.reject(error);
+  }
+);
 
 export default instance;
