7792ba99b872126b3676e4281d4ec6f021f1835d
Progetto2324NSD <review18r@gmail.com>
2024-09-04 14:38:54 +0200

    Update NavbarLogo.jsx

--- frontend/src/components/NavbarLogo.jsx
+++ frontend/src/components/NavbarLogo.jsx
@@ -1,25 +1,72 @@
-import React from "react";
-import 'bootstrap/dist/css/bootstrap.min.css';
-import { Navbar, Container } from "react-bootstrap";
-import Logo from "../utils/images/logo.png"
+import Logo from "../utils/images/logo.png";
+import { Navbar, Container, Button, Offcanvas, Nav } from "react-bootstrap";
+import { useState } from 'react';
+import "boxicons";
+import "../pages/stile/style.css";
 
-function NavbarLogo (){
-    return (
-        <Navbar className="bg-body-tertiary">
-        <Container>
-          <Navbar.Brand href="#home">
-            <img
-              alt="Logo"
-              src={Logo}
-              width="35"
-              height="35"
-              className="d-inline-block align-top"
-            />{' '}
-            Run Tracker
-          </Navbar.Brand>
-        </Container>
-      </Navbar>
-    );
+function NavbarLogo() {
+
+  const [show, setShow] = useState(false);
+
+  const handleClose = () => setShow(false);
+  const handleShow = () => setShow(true);
+
+  return (
+    <Navbar className="bg-body-tertiary">
+      <Container>
+        <Navbar.Brand href="#home">
+          <img
+            alt=""
+            src={Logo}
+            width="30"
+            height="30"
+            className="d-inline-block align-top"
+          />{' '}
+          React Bootstrap
+        </Navbar.Brand>
+        <Button variant="primary" onClick={handleShow}>
+          Menu
+        </Button>
+
+        <Offcanvas show={show} onHide={handleClose}>
+          <Offcanvas.Header closeButton>
+            <Offcanvas.Title>Menu</Offcanvas.Title>
+          </Offcanvas.Header>
+          <Offcanvas.Body>
+            <div className="user-info mb-4">
+              <img
+                src="https://via.placeholder.com/50"
+                alt="User Avatar"
+                className="rounded-circle"
+                width="50"
+                height="50"
+              />
+              <div>
+                <h5>Nome Utente</h5>
+                <p>email@esempio.com</p>
+              </div>
+            </div>
+            
+            {/* Sezione voci del menu */}
+            <Nav className="flex-column">
+              <Nav.Link href="#home">
+                <box-icon name="home" /> Home
+              </Nav.Link>
+              <Nav.Link href="#profile">
+                <box-icon name="user" /> Profile
+              </Nav.Link>
+              <Nav.Link href="#settings">
+                <box-icon name="cog" /> Settings
+              </Nav.Link>
+              <Nav.Link href="#logout">
+                <box-icon name="log-out" /> Logout
+              </Nav.Link>
+            </Nav>
+          </Offcanvas.Body>
+        </Offcanvas>
+      </Container>
+    </Navbar>
+  );
 }
 
 export default NavbarLogo;
