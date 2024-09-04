324f8f1211470d9926d051ba0536e032a71d461e
Progetto2324NSD <review18r@gmail.com>
2024-09-04 14:38:36 +0200

    Create MapCard.jsx

---
+++ frontend/src/components/cards/MapCard.jsx
@@ -0,0 +1,23 @@
+import React from 'react';
+import { Card, CardContent, Typography, Box } from '@mui/material';
+import MapComponent from '../MapComponent'; // Assicurati che il percorso sia corretto
+
+function MapCard({ title, para, onDistanceChange }) {
+  return (
+    <Card sx={{ minWidth: 275, maxWidth: 840, boxShadow: 3, borderRadius: 2, height: 595 }}>
+      <CardContent>
+        <Typography variant="h5" component="div" gutterBottom>
+          {title}
+        </Typography>
+        <Typography variant="body2" color="text.secondary" paragraph>
+          {para}
+        </Typography>
+        <Box sx={{ height: '400px', width: '100%', borderRadius: '8px', overflow: 'hidden' }}>
+          <MapComponent onDistanceChange={onDistanceChange} />
+        </Box>
+      </CardContent>
+    </Card>
+  );
+}
+
+export default MapCard;
