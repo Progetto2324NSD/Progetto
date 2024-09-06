34fbec72676bfbc0dbb3e06f01678a1c094eccaa
Progetto2324NSD <review18r@gmail.com>
2024-09-06 14:22:58 +0200

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -1,39 +1,37 @@
-//punto di accesso al backend
 const express = require('express');
 const dotenv = require('dotenv').config();
 const colors = require('colors');
 const { errorHandler } = require('./middlewares/errorMiddleware.js');
 const cors = require('cors');
 const cookieParser = require('cookie-parser');
-
-//chiamo db.js per la connessione a MONGO DB (ricordare URL in .env)
 const connectDB = require('./config/db.js');
 
 const port = process.env.PORT || 5000;
 
-//connessione al database
+// Connessione al database
 connectDB(); 
 
-//Inizializzo l'app express
+// Inizializzo l'app express
 const app = express();
 
-//Cors per permettere le richieste da altri domini (RICORDARSI IL "credentials: true") Altrimenti non invia un caBBo
+// Cors per permettere le richieste da altri domini
 app.use(cors({
     origin: 'http://localhost:3000',
     credentials: true
 }));
 
-//Parser dei cookies
+// Parser dei cookies
 app.use(cookieParser());
 
-//Per usare il body
-app.use(express.json())
-app.use(express.urlencoded({ extended: false}))
+// Per usare il body
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
 
+// Includi le route
 app.use('/user', require('./routes/userRoutes'));
+app.use('/workout', require('./routes/workoutRoutes'));
 
+// Middleware di gestione degli errori
 app.use(errorHandler);
 
 app.listen(port, () => console.log(`Il server è in ascolto sulla porta ${port}`));
-
-console.log("Ciao a tutti belli e brutti");
