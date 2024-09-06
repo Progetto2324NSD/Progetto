a52b01afb4dc2c46e43b6ea991351cdf71b433e2
Progetto2324NSD <review18r@gmail.com>
2024-09-06 14:23:23 +0200

    Update MapCard.jsx

--- frontend/src/components/cards/MapCard.jsx
+++ frontend/src/components/cards/MapCard.jsx
@@ -1,10 +1,10 @@
 import React from 'react';
 import { Card, CardContent, Typography, Box } from '@mui/material';
-import MapComponent from '../MapComponent'; // Assicurati che il percorso sia corretto
+import MapComponent from '../LastWorkoutMap'; // Assicurati che il percorso sia corretto
 
 function MapCard({ title, para, onDistanceChange }) {
   return (
-    <Card sx={{ minWidth: 275, maxWidth: 840, boxShadow: 3, borderRadius: 2, height: 595 }}>
+    <Card sx={{ minWidth: 275, maxWidth: 840, boxShadow: 3, borderRadius: 2, height: 535 }}>
       <CardContent>
         <Typography variant="h5" component="div" gutterBottom>
           {title}
