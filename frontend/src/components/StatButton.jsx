b98e87bc2f481befc29ff93beac5cbdeacd7ceea
Progetto2324NSD <review18r@gmail.com>
2024-09-07 10:16:20 +0200

    Update StatButton.jsx

--- frontend/src/components/StatButton.jsx
+++ frontend/src/components/StatButton.jsx
@@ -1,22 +1,31 @@
-import React from "react";
-import { Box, Card, CardContent, Typography} from '@mui/material';
+import { useState } from "react";
+import { Typography} from '@mui/material';
 import '../pages/stile/style.css';
 
 
-function StatButton ({ title, icon, value, description }) {
+function StatButton({ title, icon, value, description }) {
+    const [clicked, setClicked] = useState(false);
+  
+    const handleClick = () => {
+      setClicked(!clicked);
+    };
+  
     return (
-        <div className="stat-card">
-            <div className="icon">
-                {icon}
-            </div>
-        <div>
-            <Typography variant="h5">{title}</Typography>
-            <Typography className="value">{value}</Typography>
-            <Typography className="description">{description}</Typography>
+      <div
+        className={`stat-card ${clicked ? 'clicked' : ''}`}
+        onClick={handleClick}
+      >
+        <div className="icon">
+          {icon}
         </div>
-            <div className="line-graph"></div> 
+        <div className="content">
+          <Typography variant="h5">{title}</Typography>
+          <Typography className="value">{value}</Typography>
+          <Typography className="description">{description}</Typography>
         </div>
+        <div className="line-graph"></div>
+      </div>
     );
-}
-
-export default StatButton;
+  }
+  
+  export default StatButton;
