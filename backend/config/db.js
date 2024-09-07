4ab6927d462df19ed129833e382e7a4c18ad9464
Progetto2324NSD <review18r@gmail.com>
2024-09-07 10:14:37 +0200

    Update db.js

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 
-//funzione per connettersi al DB
+//Funzione per connettersi al DB
 const connectDB = async() =>{
    try{
     const conn = await mongoose.connect(process.env.MONGO_DB_URL);
