0750ceae5c40262340c5f2060a9c99c108105998
Progetto2324NSD <review18r@gmail.com>
2024-09-07 10:15:13 +0200

    Update workoutRoutes.js

--- backend/routes/workoutRoutes.js
+++ backend/routes/workoutRoutes.js
@@ -1,11 +1,19 @@
 const express = require('express');
 const router = express.Router();
-const { createWorkout, showWorkout, lastWorkout } = require('../controllers/workoutController');
+const { createWorkout, showWorkout, lastWorkout, timeDayWorkout, distanceDayWorkout, numDayWorkout, avgDayWorkout } = require('../controllers/workoutController');
 
 const { protect } = require('../middlewares/authMiddleware');
 
+//Route pagine
 router.post('/', protect, createWorkout);
 router.get('/show-workout', protect, showWorkout);
 router.get('/last-workout', protect, lastWorkout);
+
+//Route Card
+router.get('/time-oggi', protect, timeDayWorkout);
+router.get('/distance-oggi', protect, distanceDayWorkout);
+router.get('/num-oggi', protect, numDayWorkout);
+router.get('/avg-oggi', protect, avgDayWorkout);
+
 
 module.exports = router;
