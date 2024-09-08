cbb6ebd9a7bd37af77373d8a436556edd46849e0
Progetto2324NSD <review18r@gmail.com>
2024-09-09 01:03:02 +0200

    Update Dashboard.jsx

--- frontend/src/pages/Dashboard.jsx
+++ frontend/src/pages/Dashboard.jsx
@@ -1,66 +1,71 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import Navbar from "../components/NavbarLogo";
 import { Container, Row, Col } from "react-bootstrap";
-import { useState } from "react";
-import axios from "../api_vespe/axiosConfig";
+import welcome from '../utils/images/welcome.png';
 
-//stile
+// Stile
 import "./stile/style.css";
 
-//Componenti
-import MapComponent from '../components/cards/MapCard';
+// Componenti
+import MapCard from '../components/cards/MapCard';
 import QltCard from '../components/cards/QltCard';
 import Workout from '../components/cards/WorkoutCard';
 import BnvCard from '../components/cards/BnvCard';
+import LastWorkoutMap from '../components/LastWorkoutMap';
 
 function Dashboard() {
+    const [distance, setDistance] = useState(null);
+    const [type, setType] = useState(null);
+    const [time, setTime] = useState(null);
+    const [date, setDate] = useState(null);
 
-    const [Name, setName] = useState("");
+    const handleDistanceChange = (distance) => setDistance(distance);
+    const handleTypeChange = (type) => setType(type);
+    const handleTimeChange = (time) => setTime(time);
+    const handleDateChange = (date) => setDate(date);
 
-    useEffect(() => {
-
-        const fetchUserData = async () => {
-            try{
-                const response = await axios.get('/user/data',{
-                    withCredentials: true,
-                    headers: {
-                        'Accept': 'application/json'
-                    }
-                });
-
-                if(response.status === 200){
-                    setName(response.data.name);
-                }else{
-                    console.error("Errore durante il recupero dei dati dell'utente");
-                }
-            }catch(error){
-                console.error("Errore durante il recupero dei dati dell'utente");
-            }
-        }
-        fetchUserData();
-    }, []);
+    const formatDate = (dateString) => {
+        const date = new Date(dateString);
+        const day = String(date.getDate()).padStart(2, '0');
+        const month = String(date.getMonth() + 1).padStart(2, '0'); // I mesi sono indicizzati da 0
+        const year = date.getFullYear();
+        return `${day}-${month}-${year}`;
+    };
 
     return (
         <>
             <Navbar />
             <Container>
-            <h2>Dashboard</h2>
-            <hr />
-            <Row className='row'>
-                <Col md={12} xs={12}>
-                    <BnvCard name={Name} />
-                </Col>
-            </Row>
+                <h2>Dashboard</h2>
+                <hr />
+                <Row className='row'>
+                    <Col md={12} xs={12}>
+                        <BnvCard para={'Siamo felici di averti con noi. Questo è solo l\'inizio di un viaggio entusiasmante verso i tuoi obiettivi!'} img={welcome} />
+                    </Col>
+                </Row>
                 <Row className='row'>
                     <Col md={8} xs={12}>
-                        <MapComponent title="Rivedi il tuo ultimo wokout" para="Qui puoi rivedere il tuo ultimo percorso di allenamento" />
+                        <MapCard
+                            onDistanceChange={handleDistanceChange}
+                            onTimeChange={handleTimeChange}
+                            onTypeChange={handleTypeChange}
+                            onDateChange={handleDateChange}
+
+                            title="Rivedi il tuo ultimo workout"
+                            para={
+                                distance && time && type && date 
+                                ? ( <> <strong>Distanza:</strong> {distance} km, <strong>Tempo:</strong> {time} min, <strong>Tipo:</strong> {type} , <strong>Data:</strong> {formatDate(date)} </> )
+                                : 'Caricamento delle informazioni dell\'allenamento...'
+                            }
+                        />
+
                     </Col>
                     <Col md={4} xs={12}>
-                        <QltCard title="Qualità workout" para="Continua così.." /><br></br>
+                        <QltCard title="Qualità workout mensile" para="Continua così.." /><br />
                         <Workout title="Workout"/>
                     </Col>
                 </Row>
-            </Container>    
+            </Container>
         </>
     );
 }
