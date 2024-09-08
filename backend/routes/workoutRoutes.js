38a586266ab6a740346e06b89cf65b8baae85137
Progetto2324NSD <review18r@gmail.com>
2024-09-09 01:02:30 +0200

    Update workoutRoutes.js

--- backend/routes/workoutRoutes.js
+++ backend/routes/workoutRoutes.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const router = express.Router();
-const { createWorkout, showWorkout, lastWorkout, timeDayWorkout, distanceDayWorkout, numDayWorkout, avgDayWorkout } = require('../controllers/workoutController');
+const { createWorkout, showWorkout, lastWorkout, deleteWorkout, timeDayWorkout, distanceDayWorkout, numDayWorkout, avgDayWorkout, graficoAllenamenti, graficoTempo, graficoVelocita, graficoDistanza, calcolaPunteggio } = require('../controllers/workoutController');
 
 const { protect } = require('../middlewares/authMiddleware');
 
@@ -8,12 +8,22 @@ const { protect } = require('../middlewares/authMiddleware');
 router.post('/', protect, createWorkout);
 router.get('/show-workout', protect, showWorkout);
 router.get('/last-workout', protect, lastWorkout);
+router.delete('/delete-workout/:id', protect, deleteWorkout);
+
+//Route Qualità allenamenti (Card Dashboard)
+router.get('/allenamento', protect, calcolaPunteggio);
 
 //Route Card
 router.get('/time-oggi', protect, timeDayWorkout);
 router.get('/distance-oggi', protect, distanceDayWorkout);
 router.get('/num-oggi', protect, numDayWorkout);
 router.get('/avg-oggi', protect, avgDayWorkout);
+
+//Route Grafici
+router.get('/tipo-allenamenti', protect, graficoAllenamenti);
+router.get('/tempo-allenamenti', protect, graficoTempo);
+router.get('/velocita-allenamenti', protect, graficoVelocita);
+router.get('/distanza-allenamenti', protect, graficoDistanza);
 
 
 module.exports = router;
