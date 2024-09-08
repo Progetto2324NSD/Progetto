c675ec628adbf443d058a361b4ffa09c46e8222a
Progetto2324NSD <review18r@gmail.com>
2024-09-09 01:02:51 +0200

    Update NavbarLogo.jsx

--- frontend/src/components/NavbarLogo.jsx
+++ frontend/src/components/NavbarLogo.jsx
@@ -3,14 +3,34 @@ import { Navbar, Container, Button, Offcanvas, Nav } from "react-bootstrap";
 import { useState } from 'react';
 import "boxicons";
 import "../pages/stile/style.css";
- 
+import { Link, useNavigate } from 'react-router-dom';
+import axios from '../api_vespe/axiosConfig';
+import toast from 'react-hot-toast';
+
 function NavbarLogo() {
- 
   const [show, setShow] = useState(false);
- 
+  const navigate = useNavigate();
   const handleClose = () => setShow(false);
   const handleShow = () => setShow(true);
- 
+
+  const submitHandler = async () => {
+    try {
+      await axios.delete('/user/logout', {
+        method: 'DELETE',
+        credentials: 'include'
+      });
+
+      toast.success("Logout effettuato con successo!", {
+        duration: 1500
+      });
+
+      navigate('/');
+
+    } catch (error) {
+      toast.error("Errore durante il logout");
+    }
+  }
+
   return (
     <Navbar className="bg-body-tertiary">
       <Container>
@@ -22,18 +42,17 @@ function NavbarLogo() {
             height="30"
             className="d-inline-block align-top"
           />{' '}
-          React Bootstrap
+          FitLog Running
         </Navbar.Brand>
         <Button variant="primary" onClick={handleShow}>
           Menu
         </Button>
- 
+
         <Offcanvas show={show} onHide={handleClose} className="offcanvas-fixed">
           <Offcanvas.Header closeButton>
             <Offcanvas.Title>Menu</Offcanvas.Title>
           </Offcanvas.Header>
           <Offcanvas.Body className="d-flex flex-column">
-            {/* Header del Menu con Logo */}
             <div className="sidebar-header">
               <img
                 alt="Logo"
@@ -42,34 +61,32 @@ function NavbarLogo() {
                 height="30"
                 className="d-inline-block align-top"
               />
-              <h2>FitLog</h2>
+              <h2>FitLog Running</h2>
             </div>
- 
-            {/* Sezione voci del menu */}
+
             <Nav className="flex-column flex-grow-1 menu-items">
               <Nav.Link href="#dashboard">
                 <box-icon name="home" />
-                <span>Dashboard</span>
+                <Link to="/Dashboard"><span>Dashboard</span></Link>
               </Nav.Link>
               <Nav.Link href="#projects">
                 <box-icon name="grid-alt" />
-                <span>Projects</span>
+                <Link to="/Workout"><span>Workout</span></Link>
               </Nav.Link>
               <Nav.Link href="#notifications">
                 <box-icon name="bell" />
-                <span>Notifications</span>
+                <Link to="/Statistiche"><span>Statistiche</span></Link>
               </Nav.Link>
               <Nav.Link href="#analytics">
                 <box-icon name="bar-chart-alt-2" />
-                <span>Analytics</span>
+                <Link to="/Notifiche"><span>Notifiche</span></Link>
               </Nav.Link>
- 
-              {/* Spacer per spingere Logout in fondo */}
+
               <div className="flex-grow-1"></div>
- 
+
               <Nav.Link href="#logout" className="logout-link">
                 <box-icon name="log-out" />
-                <span>Logout</span>
+                <span onClick={submitHandler}>Logout</span>
               </Nav.Link>
             </Nav>
           </Offcanvas.Body>
@@ -78,5 +95,5 @@ function NavbarLogo() {
     </Navbar>
   );
 }
- 
+
 export default NavbarLogo;
