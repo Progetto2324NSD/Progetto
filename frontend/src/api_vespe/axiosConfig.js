0ac7445bff343cd644e6dc45ad9bf481e70581e4
Progetto2324NSD <review18r@gmail.com>
2024-09-16 11:33:30 +0200

    Update axiosConfig.js

--- frontend/src/api_vespe/axiosConfig.js
+++ frontend/src/api_vespe/axiosConfig.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 // Crea un'istanza di Axios
 const instance = axios.create({
-  baseURL: 'http://localhost:5000', // URL di base per le tue API
+  baseURL: 'https://progetto.onrender.com/', // URL di base per le tue API
   headers: {
     'Content-Type': 'application/json'
   }
