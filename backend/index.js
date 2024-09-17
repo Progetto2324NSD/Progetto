99786d14ea0d4c478f25ac4d5f9886b4ca2fe650
Progetto2324NSD <review18r@gmail.com>
2024-09-17 15:23:23 +0200

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -16,7 +16,7 @@ const app = express();
 
 // Cors per permettere le richieste da altri domini
 app.use(cors({
-    origin: 'https://progetto-frontend-pcvbbo9ba-progetto2324nsds-projects.vercel.app',
+    origin: 'https://progetto-frontend.vercel.app',
     credentials: true
 }));
 
