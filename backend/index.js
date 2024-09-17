058481fcf5a037e8fdfd5d57fd1150afc5c0f6d5
Progetto2324NSD <review18r@gmail.com>
2024-09-17 11:53:38 +0200

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -16,7 +16,7 @@ const app = express();
 
 // Cors per permettere le richieste da altri domini
 app.use(cors({
-    origin: 'https://progettofrontend.onrender.com',
+    origin: 'https://progetto-frontend-4tlenlpga-progetto2324nsds-projects.vercel.app',
     credentials: true
 }));
 
